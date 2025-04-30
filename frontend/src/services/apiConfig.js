a777134b6f52478525d71cee9d73c6968759ca5a
DimuthuAro <dimuthuaro@gmail.com>
2025-04-30 10:56:37 +0530

    Frontend Navbar

--- frontend/src/services/apiConfig.js
+++
@@ -1,4 +0,0 @@
-// This file contains API configuration settings
-const API_BASE_URL = 'https://aca66e10-5f44-4dc9-b47d-533cb37890ca-00-19wlipvjwrnbc.sisko.replit.dev';
-
-export default API_BASE_URL;
